eacd674c38020bacf14ce451f2e377f46cb30489
Toxe <toxe@toxe.net>
2026-01-08 12:24:54 +0100

    Show FPS in window title

--- PixelStyle/scenes/main.gd
+++ PixelStyle/scenes/main.gd
@@ -3,6 +3,11 @@ class_name Main extends Node2D
 @export var camera: Camera2D
 
 
+func _ready() -> void:
+    create_window_title_update_timer()
+    update_window_title()
+
+
 func _unhandled_input(event: InputEvent) -> void:
     var event_mouse_button: InputEventMouseButton = event as InputEventMouseButton
     if event_mouse_button && event_mouse_button.is_pressed():
@@ -13,3 +18,14 @@ func _unhandled_input(event: InputEvent) -> void:
 func _process(delta: float) -> void:
     var camera_movement := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
     camera.position += camera_movement * 100.0 * delta
+
+
+func create_window_title_update_timer() -> void:
+    var timer := Timer.new()
+    add_child(timer)
+    timer.timeout.connect(update_window_title)
+    timer.start(1.0)
+
+
+func update_window_title() -> void:
+    get_window().title = "%s [%d FPS]" % [ProjectSettings.get_setting("application/config/name"), Performance.get_monitor(Performance.TIME_FPS)]
