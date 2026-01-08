d909a4d8ab3b1de577a422da4a16f1604aee5e2a
Toxe <toxe@toxe.net>
2026-01-08 18:33:20 +0100

    Update GUI

--- PixelStyle/scenes/gui.gd
+++ PixelStyle/scenes/gui.gd
@@ -4,12 +4,15 @@ class_name GUI extends Control
 @export var texture_rect: TextureRect
 @export var king: Sprite2D
 
+@onready var camera_label: Label = $HBoxContainer/VBoxContainer/HBoxContainer/CameraLabel
+@onready var texture_rect_label: Label = $HBoxContainer/VBoxContainer/HBoxContainer2/TextureRectLabel
+@onready var king_label: Label = $HBoxContainer/VBoxContainer/HBoxContainer3/KingLabel
+
 
 func _process(_delta: float) -> void:
-    var cam_delta := camera.get_target_position() - camera.get_screen_center_position()
-    ($CameraLabel as Label).text = "Camera: target_position: %s\nscreen_center_position: %s\ndelta: %s\nscreen_transform: %s" % [camera.get_target_position(), camera.get_screen_center_position(), cam_delta, camera.get_screen_transform().origin]
-    ($TextureRectLabel as Label).text = "TextureRect: %s\n%s" % [texture_rect.get_canvas_transform().origin, texture_rect.get_screen_transform().origin]
-    ($KingLabel as Label).text = "King: %s\n%s\n%s" % [king.global_position, king.global_transform.origin, king.get_screen_transform().origin]
+    camera_label.text = "%s\n%s\n%s" % [format_vector(camera.get_target_position()), format_vector(camera.get_screen_center_position()), format_vector(camera.get_screen_transform().origin)]
+    texture_rect_label.text = "%s\n%s" % [format_vector(texture_rect.get_canvas_transform().origin), format_vector(texture_rect.get_screen_transform().origin)]
+    king_label.text = "%s\n%s" % [format_vector(king.global_position), format_vector(king.get_screen_transform().origin)]
     queue_redraw()
 
 
@@ -20,3 +23,7 @@ func _draw() -> void:
     var cam_delta := camera.get_target_position() - camera.get_screen_center_position()
     draw_circle(camera.get_screen_transform().origin, 10, Color.TURQUOISE, false, 2)
     draw_circle(camera.get_screen_transform().origin - cam_delta, 10, Color.PURPLE, false, 2)
+
+
+func format_vector(vec: Vector2) -> String:
+    return "%.2f / %.2f" % [vec.x, vec.y]
