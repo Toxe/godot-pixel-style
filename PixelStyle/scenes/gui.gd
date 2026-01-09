85e540685d9b8c62228532704410d16ffdde2d70
Toxe <toxe@toxe.net>
2026-01-09 12:14:49 +0100

    Show window and game viewport size

--- PixelStyle/scenes/gui.gd
+++ PixelStyle/scenes/gui.gd
@@ -8,6 +8,7 @@ class_name GUI extends Control
 @onready var king_label: Label = $HBoxContainer/VBoxContainer/HBoxContainer3/KingLabel
 @onready var king_speed_slider: HSlider = $HBoxContainer/VBoxContainer/HBoxContainer4/KingSpeedSlider
 @onready var king_speed_label: Label = $HBoxContainer/VBoxContainer/HBoxContainer4/KingSpeedLabel
+@onready var window_size_label: Label = $HBoxContainer/VBoxContainer/HBoxContainer5/WindowSizeLabel
 
 
 func _ready() -> void:
@@ -19,6 +20,7 @@ func _process(_delta: float) -> void:
     camera_label.text = "%s\n%s\n%s" % [format_vector(game.camera.get_target_position()), format_vector(game.camera.get_screen_center_position()), format_vector(game.camera.get_screen_transform().origin)]
     texture_rect_label.text = "%s\n%s" % [format_vector(texture_rect.get_canvas_transform().origin), format_vector(texture_rect.get_screen_transform().origin)]
     king_label.text = "%s\n%s" % [format_vector(game.king.global_position), format_vector(game.king.get_screen_transform().origin)]
+    window_size_label.text = "%s\n%s" % [format_size(get_window().size), format_size((game.get_viewport() as SubViewport).size)]
     queue_redraw()
 
 
@@ -45,6 +47,10 @@ func _unhandled_key_input(event: InputEvent) -> void:
 
 func format_vector(vec: Vector2) -> String:
     return "%.2f / %.2f" % [vec.x, vec.y]
+
+
+func format_size(vec: Vector2i) -> String:
+    return "%d×%d" % [vec.x, vec.y]
 
 
 func _on_king_speed_slider_value_changed(value: float) -> void:
