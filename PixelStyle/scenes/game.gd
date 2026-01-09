51711265107a3b845d5defb1c7031c49ea2a1ee9
Toxe <toxe@toxe.net>
2026-01-09 13:47:57 +0100

    Rename camera

--- PixelStyle/scenes/game.gd
+++ PixelStyle/scenes/game.gd
@@ -1,6 +1,6 @@
 class_name Game extends Node2D
 
-@onready var camera: Camera2D = $Path2D/PathFollow2D/King/Camera2D
+@onready var camera: Camera2D = $Path2D/PathFollow2D/King/GameCamera
 @onready var king: Sprite2D = $Path2D/PathFollow2D/King
 @onready var path_follow: PathFollow2D = $Path2D/PathFollow2D
 
