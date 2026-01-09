5927177d3447b172fed894a9ecdfad93773f20da
Toxe <toxe@toxe.net>
2026-01-09 09:01:51 +0100

    Add F hotkey to toggle Fullscreen mode

--- PixelStyle/scenes/gui.gd
+++ PixelStyle/scenes/gui.gd
@@ -26,7 +26,12 @@ func _draw() -> void:
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
-    if event.is_action_pressed("toggle_gui"):
+    if event.is_action_pressed("toggle_fullscreen"):
+        if DisplayServer.window_get_mode() != DisplayServer.WindowMode.WINDOW_MODE_WINDOWED:
+            DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_WINDOWED)
+        else:
+            DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN)
+    elif event.is_action_pressed("toggle_gui"):
         visible = !visible
     elif event.is_action_pressed("quit"):
         get_tree().quit()
