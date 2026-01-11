995fcbaa553af8368679514775b48dcd2909e560
Toxe <toxe@toxe.net>
2026-01-11 16:59:01 +0100

    Move priest along path

--- PixelStyle/scenes/game.gd
+++ PixelStyle/scenes/game.gd
@@ -1,13 +1,18 @@
 class_name Game extends Node2D
 
-@onready var king: Sprite2D = $Path2D/PathFollow2D/King
-@onready var path_follow: PathFollow2D = $Path2D/PathFollow2D
+@onready var king: Sprite2D = $KingPath2D/PathFollow2D/King
+@onready var king_path_follow: PathFollow2D = $KingPath2D/PathFollow2D
+@onready var priest: Sprite2D = $PriestPath2D/PathFollow2D/Priest
+@onready var priest_path_follow: PathFollow2D = $PriestPath2D/PathFollow2D
 
 var king_speed := 0.1
+var priest_speed := 1.0
 
 
 func _process(delta: float) -> void:
-    path_follow.progress_ratio += delta * king_speed
+    king_path_follow.progress_ratio += delta * king_speed
+    priest_path_follow.progress += priest_speed
+
     queue_redraw()
 
 
